88e6c1f6f76071c6cc1ba0287b19e8bb773ee9c9
jrlescovar <jrlescovar@gmail.com>
2025-04-16 01:30:51 -0300

    First Tree data structure

---
+++ binaryTree/binaryTree.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+struct Tree{
+	int info;
+	struct Tree *esq, *dir;
+};typedef struct Tree tree;
+
+void localiza(tree *raiz, int info, tree **no){
+	if(raiz != NULL){
+		if(raiz->info == info)
+			*no = raiz;
+		else{
+			localiza(raiz->esq, info, &*no);
+			localiza(raiz->dir, info, &*no);
+		}
+	}
+}
+
+tree *localizaNo(tree * raiz, int infopai){
+	tree *no;
+	localiza(raiz,infopai,&no);
+	return no;
+}
+
+tree *criaNo(int info){
+    tree *raiz = (tree *)malloc(sizeof(tree));
+    raiz->info = info;
+    raiz->esq = NULL;
+    raiz->dir = NULL;
+    return raiz;
+}
+void insere(tree **raiz, int info, int infopai, char lado){
+	tree *pai;
+	if(*raiz == NULL){
+		*raiz = criaNo(info);
+	}else{
+		pai = localizaNo(*raiz, infopai);
+		if(pai != NULL){
+			if(lado == 'D' && pai->dir == NULL)
+				pai->dir = criaNo(info);
+			else
+				if(lado == 'E' && pai->esq == NULL)
+					pai->esq = criaNo(info);
+				else
+					printf("Lado Indisponivel\n\n");
+		}
+	}
+}
+int main(void){
+	tree *raiz = NULL;
+	insere(&raiz, 50, 0,' ');
+	insere(&raiz, 60, 50,'D');
+	insere(&raiz, 45, 50,'E');
+	printf("Pai - %d", raiz->info);
+	printf("\nFilho Esquerda - %d",raiz->esq->info);
+	printf("\nFilho Direita - %d",raiz->dir->info);
+}
